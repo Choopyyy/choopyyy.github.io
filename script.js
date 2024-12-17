6c451d92a9fc37da4379f69a0a08cd34749317a2
Choopyyy <sucre.loren@maimonides.edu>
2024-12-17 17:15:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ document.querySelectorAll('nav a').forEach(anchor => {
     });
 });
 
+//Array de productos
+const productos = [
+    { nombre: 'Biomecanica en Protesis Parcial Removible', descripción: 'El documento desarrolla los principios biomecanicos para el diseño funcional de una protesis parcial removible, de acuerdo a las caracteristicas anatomicas y funcionales del caso.', imagen: 'img/fuerzas-oclusales-bases-extension-distal.jpg' },
+    { nombre: 'Biomecanica en Protesis Parcial Fija', descripción: 'El documento desarrolla los principios biomecanicos para el diseño funcional de una protesis parcial removible, de acuerdo a las caracteristicas anatomicas y funcionales del caso.', imagen: 'img/fuerzas-oclusales-bases-extension-distal.jpg' },
+    { nombre: 'Clasificación de Kennedy', descripción: 'El documento desarrolla los principios biomecanicos para el diseño funcional de una protesis parcial removible, de acuerdo a las caracteristicas anatomicas y funcionales del caso.', imagen: 'img/fuerzas-oclusales-bases-extension-distal.jpg' },
+    { nombre: 'Guia de ejercicios de Protesis Parcial Removible', descripción: 'El documento desarrolla los principios biomecanicos para el diseño funcional de una protesis parcial removible, de acuerdo a las caracteristicas anatomicas y funcionales del caso.', imagen: 'img/fuerzas-oclusales-bases-extension-distal.jpg' },
+    { nombre: 'Sobre el Autor', descripción: 'El documento desarrolla los principios biomecanicos para el diseño funcional de una protesis parcial removible, de acuerdo a las caracteristicas anatomicas y funcionales del caso.', imagen: 'img/fuerzas-oclusales-bases-extension-distal.jpg' },
+];
+
+function generarProdcutos() {
+    const contenedor  = document.queSelector('.elementos.container');
+    contenedor.innerHTML = '';
+    
+
 
 //Formulario de contacto: Validación de campos antes de enviarlo.
 // Validar formulario antes de enviarlo
